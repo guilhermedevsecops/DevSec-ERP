159d330d51375aab9330fb3ad2b036e1c8bfd833
Guilherme Henrique de Sousa Jesus <guilherme.devsec@icloud.com>
2025-09-01 22:05:22 -0300

    Adicionado Query
    Adicionado query de criação de tabelas de departamento, roles e açoes, e feito vinculo entre elas

--- backend/src/main/resources/db/migration/V1__create_tables.sql
+++ backend/src/main/resources/db/migration/V1__create_tables.sql
@@ -13,3 +13,57 @@ CREATE TABLE users (
     address VARCHAR(100),
     email VARCHAR(100) UNIQUE NOT NULL
 );
+
+-- TABELA DEPARTAMENTO
+
+CREATE TABLE departament (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) UNIQUE NOT NULL
+);
+
+-- TABELA PERMISSOES
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) UNIQUE NOT NULL,
+    descricao TEXT
+);
+
+-- TABELA AÇÕES
+
+CREATE TABLE actions (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) UNIQUE NOT NULL,      
+    descricao TEXT,
+    modulo VARCHAR(50) NOT NULL            
+);
+
+-- TABELA USUARIOS PERMISSÕES
+
+CREATE TABLE users_role (
+    users_id INT NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY(users_id, role_id),
+    FOREIGN KEY(users_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(role_id) REFERENCES roles(id) ON DELETE CASCADE
+);
+
+-- TABELA PERMISSÕES AÇÕES
+
+CREATE TABLE role_action (
+    role_id INT NOT NULL,
+    action_id INT NOT NULL,
+    PRIMARY KEY(role_id, action_id),
+    FOREIGN KEY(role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY(action_id) REFERENCES actions(id) ON DELETE CASCADE
+);
+
+-- TABELAS USUARIO DEPARTAMENTO
+
+CREATE TABLE user_departament (
+    user_id INT NOT NULL,
+    departament_id INT NOT NULL,
+    PRIMARY KEY(user_id, departament_id),
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(departament_id) REFERENCES departament(id) ON DELETE CASCADE
+);
