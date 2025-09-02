c01c7beffc278acf3b3dcec2656805b465e80cbb
Guilherme Henrique de Sousa Jesus <guilherme.devsec@icloud.com>
2025-09-01 22:03:49 -0300

    Criação da tela de Cadastro
    Feito a criação do html e css, e tratamento de erros com hookforms e yup

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -5,9 +5,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PathRoutes from '../config/RoutesPath';
 
 //Paginas
-
 import NotFound from '../pages/MensagePages/NotFound';
 import Login from '../pages/login/Login';
+import Register from '../pages/register/Register';
 
 const AppRoutes = () => {
   return (
@@ -15,6 +15,7 @@ const AppRoutes = () => {
     <BrowserRouter>
     <Routes>
       <Route path={PathRoutes.commonPath.login} element={<Login/>}/>
+      <Route path={PathRoutes.commonPath.registration} element={<Register/>}/>
       <Route path={PathRoutes.commonPath.notFound} element={<NotFound/>}/>
     </Routes>
     
