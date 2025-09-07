37991be3a06af733bc612436105773d6c922d042
Guilherme Henrique de Sousa Jesus <guilherme.devsec@icloud.com>
2025-09-07 01:19:26 -0300

    Criação de Recuperação por email

---
+++ backend/src/main/java/com/devsecerp/backend/entity/passworeset/PasswordResetToken.java
@@ -0,0 +1,37 @@
+package com.devsecerp.backend.entity.passworeset;
+
+import java.time.LocalDateTime;
+
+import com.devsecerp.backend.entity.usuario.Users;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "password_reset_tokens")
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime expires_at;
+
+    @Column(nullable = false)
+    private boolean used = false;
+}
